c1c725d4ca99277bbdff7509d787a975791ed0e7
Adith98 <94737607+adithshetty@users.noreply.github.com>
2023-07-06 20:42:38 -0400

    Update to ES Module. Set up schema and routes directory structure.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose')
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import 'dotenv/config.js'
 
-const mongoString = process.env.DATABASE_URL
-mongoose.connect(mongoString);
-const database = mongoose.connection
+import routes from './routes/routes.js';
 
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-
+//Initialization
 const app = express();
 
-app.use(express.json());
+////Middlewares -> Every time a route is hit, this will run first
+app.use(express.json({ limit: "30mb", extended: "true" })); //Limiting images size to 30 mb ALSO IMPORTANT FOR parsing body requests.
+app.use(express.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+//Import Routes
+app.use('/api', routes)
+
+
+const PORT = process.env.PORT || 5000;
+//Connect to Database
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
