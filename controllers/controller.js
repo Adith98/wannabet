38d8d177d86d45b7da2dd065b1629c1a006e85bb
Adith98 <adithshetty9898@gmail.com>
2023-08-04 12:31:15 -0400

    Build dummy functions for the controller module.

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,14 +8,29 @@ import asyncHandler from "express-async-handler"
 
 
 // Add a User.
+export const add_user = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Add a User.");
+});
 
 // Update details of a User.
+export const update_user = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Update details of a User.");
+});
 
 // Add a participant as a temporary User.
+export const add_participant = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Add a participant.");
+});
 
 // Get list of Bets for a User.
+export const bet_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Get list of Bets.");
+});
 
 // Add bet information to all the participants.
+export const add_bet_info = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Add bet information to all the participants.");
+});
 
 // Place a Bet.
 export const place_bet = asyncHandler(async (req, res, next) => {
